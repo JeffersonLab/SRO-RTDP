365b4add841fe91289225cc9bfa4fe4d81399480
Xinxin Mei <xmei@jlab.org>
2023-11-08 17:08:37 -0500

    Init argument parser

---
+++ rtdp/python/rtdp.py
@@ -0,0 +1,57 @@
+# Author: xmei@jlab.org
+
+"""
+Entry point of rdtp.
+"""
+
+import argparse
+# Logging cookboook: https://docs.python.org/3/howto/logging-cookbook.html
+# import logging
+
+RTDP_CLI_APP_DESCRIP_STR = \
+    "rtdp: JLab's streaming readout RealTime Development and testing Platform."
+RTDP_CLI_APP_URL_STR = "https://github.com/JeffersonLab/SRO-RTDP"
+RTDP_CLI_APP_VERSION_STR = "0.0"
+
+def get_parser():
+    """Define the application arguments. Create the ArgumentParser object and return it.
+
+    Returns:
+        parser (argparse.ArgumentParser): The created argument parser.
+    """
+    parser = argparse.ArgumentParser(
+        prog="rtdp",
+        description=RTDP_CLI_APP_DESCRIP_STR,
+        epilog=f'Github page: {RTDP_CLI_APP_URL_STR}'
+    )
+    parser.add_argument('-v', '--version', action='version',
+        version=f'%(prog)s {RTDP_CLI_APP_VERSION_STR}')
+    parser.add_argument('config_file', nargs='?',
+        help='path to your YAML configuration file')
+    return parser
+
+
+def run_rtdp(parser):
+    """Proocess the cli inputs.
+
+    Args:
+        parser (argparse.ArgumentParser): The created argument parser.
+    """
+    args = parser.parse_args()
+    if args.config_file:
+        # TODO: this does not gaurentee the config file even exit or is a valid yaml file.
+        print(f"Starting the platform using the specified YAML configuration file:\
+            {args.config_file}")
+    else:
+        parser.print_help()
+
+
+def main():
+    """
+    The main function to start the application.
+    """
+    run_rtdp(get_parser())
+
+
+if __name__ == '__main__':
+    main()
