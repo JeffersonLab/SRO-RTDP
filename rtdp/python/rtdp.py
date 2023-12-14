bf01b501b5f5626728a9944e949fcfc6e00d52bc
Xinxin (Cissie) Mei <40411327+cissieAB@users.noreply.github.com>
2023-12-14 16:52:57 -0500

    Add graphviz/dash code to visualize ersap configuration yml file (#27)
    * Add graphviz code to visualize ersap configuration yml file
    
    Also add some validation rules for reading the ERSAP yaml configuration file.
    
    * Add the demo ERSAP configuration file
    
    * Add dash_cytoscape for interactive display.
    
    Open a Dash page at port 8050. It's not an interactive page now.

--- rtdp/python/rtdp.py
+++ rtdp/python/rtdp.py
@@ -8,6 +8,12 @@ import argparse
 # Logging cookboook: https://docs.python.org/3/howto/logging-cookbook.html
 # import logging
 
+# Dash modules
+from dash import html, Dash, dcc
+import dash_cytoscape as cyto
+
+from config_parser import ERSAPReader
+
 RTDP_CLI_APP_DESCRIP_STR = \
     "rtdp: JLab's streaming readout RealTime Development and testing Platform."
 RTDP_CLI_APP_URL_STR = "https://github.com/JeffersonLab/SRO-RTDP"
@@ -39,19 +45,29 @@ def run_rtdp(parser):
     """
     args = parser.parse_args()
     if args.config_file:
-        # TODO: this does not gaurentee the config file even exit or is a valid yaml file.
         print(f"Starting the platform using the specified YAML configuration file:\
             {args.config_file}")
+        # TODO: using ERSAP reader here. Should be generalized.
+        # TODO: not a service launching yet.
+        configurations = ERSAPReader(args.config_file)
+        # configurations.print_cytoscape_elements()
+
+        app = Dash(__name__)
+
+        app.layout = html.Div([
+            html.H1(children='Visulization of the ERSAP configuration file', style={'textAlign':'Left'}),
+            cyto.Cytoscape(
+                id='cyto-display',
+                layout={'name': 'grid'},
+                style={'width': '1200px', 'height': '400px'},
+                elements=configurations.get_cytoscape_elements()
+            )
+        ])
+
+        app.run_server(debug=True)
     else:
         parser.print_help()
 
 
-def main():
-    """
-    The main function to start the application.
-    """
-    run_rtdp(get_parser())
-
-
 if __name__ == '__main__':
-    main()
+    run_rtdp(get_parser())
