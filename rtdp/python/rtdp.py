09e890a914c9fa6de716ab7b20711128174984b4
Xinxin (Cissie) Mei <40411327+cissieAB@users.noreply.github.com>
2023-12-18 10:43:28 -0500

    Feature: show details of the selected ERSAP config node (#29)
    - Separate Dash-related context to a submodule rtdp_dash_cyto.
    - Add an interactive function display_tap_config_node to show the details of the tapped node by the Web user.

--- rtdp/python/rtdp.py
+++ rtdp/python/rtdp.py
@@ -8,11 +8,8 @@ import argparse
 # Logging cookboook: https://docs.python.org/3/howto/logging-cookbook.html
 # import logging
 
-# Dash modules
-from dash import html, Dash, dcc
-import dash_cytoscape as cyto
-
-from config_parser import ERSAPReader
+from rtdp_config_parser import ERSAPReader
+from rtdp_dash_cyto import get_dash_app
 
 RTDP_CLI_APP_DESCRIP_STR = \
     "rtdp: JLab's streaming readout RealTime Development and testing Platform."
@@ -23,7 +20,7 @@ def get_parser():
     """Define the application arguments. Create the ArgumentParser object and return it.
 
     Returns:
-        parser (argparse.ArgumentParser): The created argument parser.
+    - parser (argparse.ArgumentParser): The created argument parser.
     """
     parser = argparse.ArgumentParser(
         prog="rtdp",
@@ -41,29 +38,16 @@ def run_rtdp(parser):
     """Proocess the cli inputs.
 
     Args:
-        parser (argparse.ArgumentParser): The created argument parser.
+    - parser (argparse.ArgumentParser): The created argument parser.
     """
     args = parser.parse_args()
     if args.config_file:
-        print(f"Starting the platform using the specified YAML configuration file:\
-            {args.config_file}")
         # TODO: using ERSAP reader here. Should be generalized.
         # TODO: not a service launching yet.
         configurations = ERSAPReader(args.config_file)
-        # configurations.print_cytoscape_elements()
+        ersap_nodes = configurations.get_flowchart_nodes()
 
-        app = Dash(__name__)
-
-        app.layout = html.Div([
-            html.H1(children='Visulization of the ERSAP configuration file', style={'textAlign':'Left'}),
-            cyto.Cytoscape(
-                id='cyto-display',
-                layout={'name': 'grid'},
-                style={'width': '1200px', 'height': '400px'},
-                elements=configurations.get_cytoscape_elements()
-            )
-        ])
-
+        app = get_dash_app(ersap_nodes)
         app.run_server(debug=True)
     else:
         parser.print_help()
