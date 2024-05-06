10c34362f43ea9ccd58da9b0e7c75a6db7556ead
David Lawrence <davidl@jlab.org>
2024-05-06 09:34:27 -0400

    Initial files for rtdp-gluex docker container image.

---
+++ containers/rtdp-gluex/README.md
@@ -0,0 +1,24 @@
+# RTDP Container Image for GlueX Monitoring
+
+This container will build the necessary software components to implement the online monitoring system used in Hall-D. This provides an ET + cMsg + RootSpy enabled software suite that can be used to read events from a remote ET system and generate PNG files suitable for Hydra consumption.
+
+To build the image with Docker:
+
+~~~bash
+docker build -t rtdp-gluex -f Dockerfile .
+~~~
+
+To run the cMsg server (n.b. this runs it in the background):
+
+~~~bash
+docker run -it --rm --net host rtdp-gluex  bash
+source /opt/setenv.sh 
+java -cp java/jars/java8/cMsg-5.2.jar -server -Dtimeorder -Ddebug=info org/jlab/coda/cMsg/cMsgDomain/server/cMsgNameServer >& /dev/null &
+~~~
+
+To run the hdmon program in the same container:
+
+~~~bash
+export ROOTSPY_UDL="cMsg://127.0.0.1/cMsg/rootspy"
+hdmon ET:/tmp/et_mon
+~~~
---
+++ containers/rtdp-gluex/setenv.sh
@@ -0,0 +1,29 @@
+
+
+source /opt/build_scripts/gluex_env_boot_jlab.sh --bs /opt/build_scripts
+gxenv /opt/version.xml
+
+cd /opt/root
+source bin/thisroot.sh
+
+export NTHREADS=12
+export JAVA_HOME=/usr/lib/jvm/default-java
+export ETINSTALL=${GLUEX_TOP}/et/et-16.3
+export ETROOT=${ETINSTALL}/Linux-x86_64
+export CODA=$GLUEX_TOP/coda
+export CMSGROOT=$CODA/Linux-x86_64
+export ROOTSPY=$GLUEX_TOP/rootspy/latest/${BMS_OSNAME}
+export HALLD_RECON_HOME=$GLUEX_TOP/halld_recon/latest
+
+export PATH=${PATH}:${GLUEX_TOP}/hddm/hddm/bin:${GLUEX_TOP}/hdds/hdds-*/Linux_Ubuntu20.04-x86_64-gcc9.4.0/bin
+export PATH=${PATH}:${GLUEX_TOP}/monitoring/${BMS_OSNAME}/bin
+export PATH=${PATH}:${GLUEX_TOP}/etUtils/${BMS_OSNAME}/bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$GLUEX_TOP/et/et-16.3/Linux-x86_64/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$GLUEX_TOP/coda/Linux-x86_64/lib/
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
+
+source $GLUEX_TOP/jana/latest/${BMS_OSNAME}/setenv.sh 
+
+export ROOTSPY_UDL="cMsg://127.0.0.1/cMsg/rootspy"
+
+unset CODA
