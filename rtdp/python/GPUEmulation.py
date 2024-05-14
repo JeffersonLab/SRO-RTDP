b4f24f7e2e6374aeb96a4363f65e247f5094b09f
AyanRoy16 <ayan@sciayan-mac.jlab.org>
2024-05-14 12:38:00 -0400

    Adding the workflow of data generation, processing and writing into a file

--- rtdp/python/GPUEmulation.py
+++ rtdp/python/GPUEmulation.py
@@ -1,12 +1,14 @@
 import numpy as np
-import time,sys
+import time,sys,random
 
 class GPU:
-    
-    def __init__(self,data,mean,deviation):
+    __random_delay = None
+    __sending_delay = 3 # delay to send the data to the CPU
+
+    def __init__(self,data,minDelay,maxDelay):
         self.data = data 
-        self.mean = mean 
-        self.deviation = deviation 
+        self.minDelay = minDelay 
+        self.maxDelay = maxDelay 
 
     def progress_bar(self,duration):
         start_time = time.time()
@@ -21,40 +23,54 @@ class GPU:
             if progress >= 1.0:
                 break
             time.sleep(0.1)  # Update progress every 0.1 seconds
-        print("\n")
+        print ()
 
     def processingDelay(self):
-        random_delay = np.random.normal(self.mean, self.deviation)
-        # Ensure the random delay is non-negative (delay cannot be negative)
-        random_delay = max(0, random_delay)
-        self.progress_bar(random_delay) # time spend inside of the GPU
+        self.random_delay = random.uniform(self.minDelay, self.maxDelay)
+        self.progress_bar(self.random_delay) # time spend inside of the GPU
         return 
-
+    
+    def sendDataToCPU(self):
+        scaleFactor = np.random.uniform(2, 4, size=(3, 4)) # row is 3 and col is 4 which is hardcoded for the POC
+        modified_data = self.data * scaleFactor
+        output_file = 'scaled_data.txt'
+        with open(output_file, 'a') as file:
+            # Append a header if needed
+            file.write("Data Block :\n")
+            # Append the scaled data using np.savetxt
+            np.savetxt(file, modified_data, fmt='%.6f', delimiter=' ')
+        time.sleep(self.__sending_delay)
+        return 
+    
     def gpu_Component(self):
-        print ("\t\t Obtained Data from CPU \t\t \n")
-        print ("\t\t Processing the data \t\t \n")
+        print ("\t\t Obtained Data from CPU \t\t ")
+        print ("\t\t Processing the data \t\t ")
         self.processingDelay()
-        print (" \t\t Processed data. Sending data back to CPU component \t\t\n")
+        print (" \t\t Processed data. Sending data back to CPU component \t\t")
+        self.sendDataToCPU()
         return 
 
 class CPU:
-    
-    def __init__(self,data, mean, deviation):
-        self.data = data
-        self.mean = mean
-        self.deviation = deviation
+    __sendingDelay = 4 # delay to send the data to the GPU
+    def __init__(self,seed, minDelay, maxDelay):
+        self.seed = seed
+        self.minDelay = minDelay
+        self.maxDelay = maxDelay
 
     def cpu_component(self):
-        print ("\t\tInside of the CPU component\t\t\n")
-        gpucomponent = GPU(self.data, self.mean, self.deviation)
-        gpucomponent.gpu_Component()
-        print ()
+        while True:
+            rows , columns  = 3,4
+            data = np.random.rand(rows,columns)
+            gpucomponent = GPU(data, self.minDelay, self.maxDelay)
+            print ("\t\t Sending data to Emulated GPU")
+            time.sleep(self.__sendingDelay)
+            gpucomponent.gpu_Component()
+            print ("---------------------- Received data from the GPU emulator ------------------ \n\n")
         return
 
 if __name__ == "__main__" :
-    data = {1,2,3,4,5}
-    meanDelay = input ("Enter the mean delay: ")
-    stdDeviationDelay = input ("Enter the standard deviation of the delay: ")
-    cpucomponent = CPU(data, int(meanDelay), int(stdDeviationDelay))
+    seed = 10
+    minDelay = input ("Enter the min delay: ")
+    maxDelay = input ("Enter the max delay: ")
+    cpucomponent = CPU(seed, int(minDelay), int(maxDelay))
     cpucomponent.cpu_component()
-    print ("\t\t Data received from emulated GPU \t\t \n")
