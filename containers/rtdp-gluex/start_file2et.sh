86ac3738fb31b7f787926eeaba722607bbd1dce3
David Lawrence <davidl@jlab.org>
2024-05-19 14:31:09 -0400

    Snapshotting.

---
+++ containers/rtdp-gluex/start_file2et.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Directory with evio file and evio file
+# (make symlink in local diractory and assume it is bound to /work)
+export EVIO_FILE_FULLPATH=/Users/davidl/work/2024.02.23.DataReduction/hd_rawdata_121090_050_100MB.evio
+export EVIO_FILE_DIR=`dirname $EVIO_FILE_FULLPATH`
+export EVIO_FILE=`basename $EVIO_FILE_FULLPATH`
+
+# Get IP address of docker container running et_start
+# export ET_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' et_server`
+export ET_HOST=localhost
+
+# Command to run the cMsg server inside the container
+export GLUEX_TOP=/group/halld/Software/builds/Linux_Ubuntu20.04-x86_64-gcc9.4.0
+export CMD="file2et -H ${ET_HOST} -p 11111 -loop -f /work/et_sys_mon /data/${EVIO_FILE}"
+
+# Run in foreground
+# exec docker exec -it et_server ${CMD}
+# exec docker exec -it et_server /bin/bash -c "source /opt/setenv.sh && file2et -f /work/et_sys_mon /work/${EVIO_FILE}"
+exec docker run -v ${PWD}:/work -v ${EVIO_FILE_DIR}:/data -it --rm --net host --name file2et rtdp-gluex:latest ${CMD}
+
+
---
+++ containers/rtdp-gluex/start_hdmon.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Command to run the cMsg server inside the container
+export GLUEX_TOP=/group/halld/Software/builds/Linux_Ubuntu20.04-x86_64-gcc9.4.0
+# export CMD="hdmon -PPLUGINS=occupancy_online ET:/work/et_sys_mon:MON:localhost:11111"
+# export CMD="hdmon -PPLUGINS=occupancy_online ET:/work/et_sys_mon:MON:192.168.65.3:11111"
+# export CMD="hdmon -PPLUGINS=occupancy_online ET:/work/et_sys_mon:MON:127.0.0.1:11111"
+export CMD="hdmon -PPLUGINS=occupancy_online ET:/work/et_sys_mon:MON:192.168.65.6:11111"
+# export CMD="hdmon -PPLUGINS=occupancy_online ET:/work/et_sys_mon:MON:239.200.0.0:11111"
+
+
+# Run in foreground
+# exec docker run -v ${PWD}:/work -it --rm --net host --name hdmon rtdp-gluex:latest ${CMD}
+exec docker run -it --rm --net host --name hdmon rtdp-gluex:latest ${CMD}
+# exec docker run -v ${PWD}:/work -it --rm --name hdmon rtdp-gluex:latest ${CMD}
+
+
+
