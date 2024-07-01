980e1d677161db9d54bb39db5d29b2497fc6a3f4
Xinxin Mei <xmei@jlab.org>
2024-07-01 16:44:23 -0400

    Complete get_cont_id part
    Test with daosfs08, 2 containers.

--- rtdp/python/nodegraph/demo_fabric_nvidarm-adm.py
+++ rtdp/python/nodegraph/demo_fabric_nvidarm-adm.py
@@ -1,4 +1,6 @@
 import time
+import json
+from typing import List
 from fabric import Connection
 
 # Define the remote host and other details
@@ -6,8 +8,9 @@ hostname = 'daosfs08.jlab.org'
 username = 'xmei'
 private_key = '/home/xmei/.ssh/id_ed25519'  # absolute path only
 
-CONT_ID = 'b24776a86238'  # first 12 digits of the full container id
 UID = '11066'
+
+IMGAE_ID = '8d91d8e4ec20'  # first 12 digits of the image sha256
 
 REMOTE_FILE_PATH_CONT_STATS = '/tmp/fetch_cont_stats.sh'
 LOCAL_FILE_PATH_CONT_STATS = 'fetch_cont_stats_rhel.sh'
@@ -36,9 +39,28 @@ def upload_file(local_path, remote_path):
     conn.close()
 
 
-def get_all_cont_ids():
+def get_cont_ids(raw_input: str, image_list=[IMGAE_ID]) -> List[str]:
+    """
+
+    Args:
+        - raw_input: the stdout of the command. 
+    """
+    res = []
+    json_list = json.loads(raw_input)
+    for item in json_list:
+        # E.g. 'ImageID': 'sha256:8d91d8e4ec20f35673952c1be3dd7c375a76eddef015db92318c9ca56548d9e5'
+        if item['ImageID'][7:19] in image_list:
+            res.append(
+                item['Id'][:12],   # E.g. 'Id': '3a48dcef87fa5fb0ef6a53baae82ff98e76d76a171c3a8b6ce6e4fb5904af2e4'
+                )
+    # print(res)
+    return res
+
+
+def get_all_cont_ids_raw():
     conn = Connection(host=hostname, user=username, connect_kwargs={'key_filename': private_key})
 
+    # 'warn=True' to ignore non-zero exit codes
     # 'hide=True' so it will not print out the results
     result = conn.run(f'sh {REMOTE_FILE_PATH_CONT_IDS}', warn=True, hide=True)
 
@@ -46,16 +68,17 @@ def get_all_cont_ids():
 
     # Print the command output and errors
     if result.ok:
+        # print(result)
+        # print(result.stdout)
         return result.stdout.strip()
     else:
         print("Error output:")
         print(result.stderr.strip())
 
 
-def fetch_container_stats(c, remote_file_path, container_id):
+def fetch_single_container_stats(c, remote_file_path, container_id):
     # Run the script remotely
-    result = c.run(f'sh {remote_file_path} {container_id}', warn=True)  # 'warn=True' to ignore non-zero exit codes
-
+    result = c.run(f'sh {remote_file_path} {container_id}', warn=True)  
     # Print the command output and errors
     if result.ok:
         return result.stdout.strip()
@@ -71,15 +94,17 @@ if __name__ == "__main__":
     upload_file(LOCAL_FILE_PATH_CONT_IDS, REMOTE_FILE_PATH_CONT_IDS)
     upload_file(LOCAL_FILE_PATH_CONT_STATS, REMOTE_FILE_PATH_CONT_STATS)
 
-    get_all_cont_ids()
+    time.sleep(2)
+
+    target_cont_ids = get_cont_ids(get_all_cont_ids_raw())
 
     conn = Connection(hostname, user=username, connect_kwargs={'key_filename': private_key})
     print(f"Connected to {hostname}")
 
-    # Forwarding the container stats
-    for _ in range(10):
-        fetch_container_stats(conn, REMOTE_FILE_PATH_CONT_STATS, CONT_ID)
-        time.sleep(10)
+    # # Forwarding the container stats
+    # for _ in range(10):
+    #     fetch_container_stats(conn, REMOTE_FILE_PATH_CONT_STATS, CONT_ID)
+    #     time.sleep(10)
 
     # Delete remote files
     delete_remote_file(REMOTE_FILE_PATH_CONT_IDS)
