7a34099273e64c6baf9bdef2afe763dd89f4bcd8
Michael Goodrich <goodrich@jlab.org>
2025-03-24 18:13:41 -0400

    Allow user to pass in destination ip/port

--- rtdp/cpp/cpu_emu/zmq-event-clnt.cc
+++ rtdp/cpp/cpu_emu/zmq-event-clnt.cc
@@ -3,18 +3,69 @@
 //  Connects REQ socket to tcp://localhost:5555
 //  Sends 10MB "event" to server, expects back
 //
+#include <unistd.h>
 #include <zmq.hpp>
 #include <string>
 #include <iostream>
+#include <netinet/in.h>
 
-int main ()
+using namespace std;
+using namespace zmq;
+using namespace chrono;
+
+#define DBG 0	//print extra verbosity apart from -v switch
+    
+void   Usage()
 {
+    char usage_str[] =
+        "\nUsage: \n\
+        -h help  \n\
+        -i destination address (string)  \n\
+        -p destination port  \n\n";
+
+    cout <<  usage_str;
+}
+
+
+int main (int argc, char *argv[])
+{
+    int optc;
+
+    bool     psdI=false, psdP=false;
+    char     dst_ip[INET6_ADDRSTRLEN] = "127.0.0.1";	// target ip
+    uint16_t dst_prt = 0; // target port
+
+    while ((optc = getopt(argc, argv, "hi:p:")) != -1)
+    {
+        switch (optc)
+        {
+        case 'h':
+            Usage();
+            exit(1);
+        case 'i':
+            strcpy(dst_ip, (const char *) optarg) ;
+            psdI = true;
+            if(DBG) cout << " -i " << dst_ip;
+            break;
+        case 'p':
+            dst_prt = (uint16_t) atoi((const char *) optarg) ;
+            psdP = true;
+            if(DBG) cout << " -p " << dst_prt;
+            break;
+        case '?':
+            cout << "Unrecognised option: " << optopt;
+            Usage();
+            exit(1);
+        }
+    }
+
+    if(!(psdI && psdP)) {Usage(); exit(1);}
     //  Prepare our context and socket
     zmq::context_t context (1);
     zmq::socket_t socket (context, zmq::socket_type::req);
 
     std::cout << "Connecting to server..." << std::endl;
-    socket.connect ("tcp://ejfat-fs-daq:5555");
+    socket.connect (string("tcp://") + dst_ip + ':' +  to_string(dst_prt));
 
     //  Do 10 requests, waiting each time for a response
     for (int request_nbr = 0; request_nbr != 2; request_nbr++) {
