27bef25c61ea360a624ca794e15f46c29939a261
Xinxin Mei <xmei@jlab.org>
2025-04-01 00:34:57 -0400

    Adjust Python sender rate logger

--- rtdp/cuda/gpu_proxy/python_zmq_helper/zmq_fp_sender.py
+++ rtdp/cuda/gpu_proxy/python_zmq_helper/zmq_fp_sender.py
@@ -30,7 +30,6 @@ import numpy as np
 
 
 DATA_NUMPY_WIDTH = 20480000  # Sending data grouped by this dimension. 40960000 does not work!
-BYTES_PER_MESSAGE = DATA_NUMPY_WIDTH * 4  # float32 = 4 bytes
 
 def main():
     parser = argparse.ArgumentParser(description="ZMQ Floating Point Data Sender")
@@ -40,13 +39,18 @@ def main():
                         "--port", type=int, default=55555, help="Port number to send data to (default: 55555)")
     parser.add_argument("-i",
                         "--all-ones", action="store_true", help="Send all ones if enabled (default: random values)")
-    parser.add_argument("--rate", type=float, default=500.0,
+    parser.add_argument(
+                        "--group-size", type=int, default=DATA_NUMPY_WIDTH,
+                        help=f"The number of float number each ZMQ msg contains (default: {DATA_NUMPY_WIDTH})")
+    parser.add_argument("-r",
+                        "--rate", type=float, default=500.0,
                         help="Average MB/s to send (default = 500)")
     
     args = parser.parse_args()
     
     context = zmq.Context()
     socket = context.socket(zmq.PUSH)
+    socket.setsockopt(zmq.SNDHWM, 1000)  # set send high-water mark to 1000 messages
     socket.connect(f"tcp://{args.ip_addr}:{args.port}")
     
     print(f"Sending data to {args.ip_addr}:{args.port} {'(all ones)' if args.all_ones else '(random values)'}")
@@ -55,10 +59,9 @@ def main():
         print(f"Target send rate: {args.rate} MB/s\n")
 
     # Calculate time interval between sends to maintain the desired MB/s rate
-    interval = 0
     if args.rate > 0:
-        interval = max(1.0, BYTES_PER_MESSAGE / (args.rate * 1e6))
-        print(f"Sent interval: {interval * 1000} ms")
+        interval = (args.group_size * 4.0) / (args.rate * 1e6)
+        print(f"Each message needs: {interval * 1000} ms")
     else:
         print(f"--rate should be a positive number!!!")
         exit(-1)
@@ -67,23 +70,24 @@ def main():
 
     try:
         while True:
+            start = time.time()
             if args.all_ones:
-                data = np.ones(DATA_NUMPY_WIDTH, dtype=np.float32)
+                data = np.ones(args.group_size, dtype=np.float32)
             else:
-                data = np.random.rand(DATA_NUMPY_WIDTH).astype(np.float32)
+                data = np.random.rand(args.group_size).astype(np.float32)
 
-            socket.send(data.tobytes())
+            socket.send(data.tobytes())  # blocks until all data is sent
+            duration = time.time() - start  # data is handled to ZMQ queue but not fully sent out
 
-            if interval > 0:
-                next_send_time += interval
-                now = time.perf_counter()
-                sleep_duration = next_send_time - now
-                if sleep_duration > 0:
-                    print(f"\tSent {data.nbytes / 1e6} MB, sleep for {sleep_duration * 1000} ms...")
-                    time.sleep(sleep_duration)  
-                else:
-                    # Falling behind: skip sleep to catch up
-                    next_send_time = now
+            remaining = 1.0 - duration
+            if remaining > 0:
+                print(f"\tSent {data.nbytes / 1e6} MB, ",
+                    f"scurr_send_rate={max(args.rate, data.nbytes / (duration * 1e6))} MB/s, duration={duration * 1000} ms")
+                print(f"\tSleep for {remaining * 1000} ms...\n")
+                time.sleep(remaining)
+            else:
+                print(f"\tSent {data.nbytes / 1e6} MB, curr_send_rate={data.nbytes / (duration * 1e6)} MB/s, duration={duration * 1000} ms")
+
     except KeyboardInterrupt:
         print("\nTerminating sender.")
     finally:
