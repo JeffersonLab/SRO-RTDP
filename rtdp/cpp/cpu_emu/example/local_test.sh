a5ffbff1578e2de9688759a717adab3af0e60d19
tsaie79 <tsaie79@gmail.com>
2025-04-20 01:13:22 +0000

    Update emulator configuration: reduce processing time by using single thread, lower latency, and smaller memory footprint

--- rtdp/cpp/cpu_emu/example/local_test.sh
+++ rtdp/cpp/cpu_emu/example/local_test.sh
@@ -80,7 +80,7 @@ run_component "Receiver" "apptainer run --pwd /app $SIF_FILE receiver -z -i 127.
 sleep 2
 
 # Start the emulator
-run_component "Emulator" "apptainer run --pwd /app $SIF_FILE emulator -i 127.0.0.1 -p $EMULATOR_SND_PORT -r $EMULATOR_RCV_PORT -t 2 -b 500 -m 0.05 -o 0.001 -s 0 -v 2"
+run_component "Emulator" "apptainer run --pwd /app $SIF_FILE emulator -i 127.0.0.1 -p $EMULATOR_SND_PORT -r $EMULATOR_RCV_PORT -t 1 -b 100 -m 0.01 -o 0.001 -s 0 -v 2"
 
 # Wait for emulator to start
 sleep 2
