6256482ef27d8d8bd2b41ac0d742eb219cf27529
Michael Goodrich <goodrich@jlab.org>
2025-04-25 12:18:27 -0400

    Fix network serialization/deserialization issues

--- rtdp/cpp/cpu_emu/buffer_packet.hh
+++ rtdp/cpp/cpu_emu/buffer_packet.hh
@@ -6,25 +6,32 @@
 #include <zmq.hpp>
 
 struct BufferPacket {
-    size_t size;
-    double timestamp;
+    uint32_t size;
+    double   timestamp;
     uint32_t stream_id;
 
-    static constexpr size_t PACKET_SIZE = sizeof(size_t) + sizeof(double) + sizeof(uint32_t);
+    static constexpr size_t PACKET_SIZE = sizeof(uint32_t) + sizeof(double) + sizeof(uint32_t);
 
     void serialize(char* buffer) const {
+        uint32_t size_be      = htonl(size);
+        double timestamp_be   = timestamp; // htonl(timestamp); ????
         uint32_t stream_id_be = htonl(stream_id);
-        std::memcpy(buffer, &size, sizeof(size));
-        std::memcpy(buffer + sizeof(size), &timestamp, sizeof(timestamp));
-        std::memcpy(buffer + sizeof(size)+sizeof(timestamp), &stream_id_be, sizeof(stream_id_be));
+        std::memcpy(buffer,                                        &size_be,      sizeof(size_be));
+        std::memcpy(buffer + sizeof(size_be),                      &timestamp_be, sizeof(timestamp_be));
+        std::memcpy(buffer + sizeof(size_be)+sizeof(timestamp_be), &stream_id_be, sizeof(stream_id_be));
     }
 
     static BufferPacket deserialize(const char* buffer) {
         BufferPacket pkt;
+        uint32_t size_be;
+        double timestamp_be;
         uint32_t stream_id_be;
-        std::memcpy(&pkt.size, buffer, sizeof(size));
-        std::memcpy(&pkt.timestamp, buffer + sizeof(size), sizeof(timestamp));
-        std::memcpy(&stream_id_be, buffer + sizeof(size)+sizeof(timestamp), sizeof(stream_id_be));
+        std::memcpy(&size_be,      buffer,                                        sizeof(size_be));
+        std::memcpy(&timestamp_be, buffer + sizeof(size_be),                      sizeof(timestamp_be));
+        std::memcpy(&stream_id_be, buffer + sizeof(size_be)+sizeof(timestamp_be), sizeof(stream_id_be));        
+        
+        pkt.size      = ntohl(size_be);
+        pkt.timestamp = timestamp_be; //ntohl(timestamp_be); ???
         pkt.stream_id = ntohl(stream_id_be);
         return pkt;
     }
