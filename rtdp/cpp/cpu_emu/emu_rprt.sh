2c755c10a09837b4e0033b3160d3cd7b2cb6e8ef
Mike Goodrich <goodrich@jlab.org>
2025-06-02 15:57:30 -0400

    add timestamp sorted trace

--- rtdp/cpp/cpu_emu/emu_rprt.sh
+++ rtdp/cpp/cpu_emu/emu_rprt.sh
@@ -125,10 +125,10 @@ echo "t11 $t11"
 echo "t12 $t12"
 echo "t13 $t13"
 
-set -m
-
 #search for missed frames
-#tf=$(mktemp); grep emulate_stream $t|grep -Ev "^\[emulate_stream" |grep -v emulate_sender-zmq > $tf
-#grep 700 $t|grep -Ev "^\[cpu_emu"|grep -v launcher_py_cpu_emu|grep -v emulate_stream >> $tf
-#sort -n -k1 $tf|grep -v Estimated|grep -v Measured|grep -v recd|grep -v Memory|less
+tf=$(mktemp); grep emulate_stream $t|grep -Ev "^\[emulate_stream" |grep -v emulate_sender-zmq > $tf
+grep 700 $t|grep -Ev "^\[cpu_emu"|grep -v launcher_py_cpu_emu|grep -v emulate_stream >> $tf
+sort -n -k1 $tf|grep -v Estimated|grep -v Measured|grep -v recd|grep -v Memory|less
 # then compare 7003 to 7002, etc.
+
+set -m
