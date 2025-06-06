b69a52dcaaba28396054db264024afa8e3050298
Michael Goodrich <goodrich@jlab.org>
2025-06-06 16:23:58 -0400

    Parameterize for arbitrary number of components and resequence component  numbering

--- rtdp/cpp/cpu_emu/sim_rprt.sh
+++ rtdp/cpp/cpu_emu/sim_rprt.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #set -x
-# ./sim_rprt <trace_file>
+# ./sim_rprt <trace_file> <num_components> <base_port>
+num_cmp=$2
+base_port=$3
+hi_port=$(echo "$base_port + $num_cmp - 1" | bc)
+echo "num_cmp = $num_cmp"
+echo "base_port = $base_port"
+echo "hi_port = $hi_port"
 
 #================= run setup/execution/cleanup =====================
 #killall cpu_sim python zmq-event-clnt; 
@@ -30,33 +36,23 @@ grep -i "\[simulate_stream:] Sending frame" $t|cut -f 1,7 -d' ' > $t0c
 cat $t0c|gnuplot -p -e "set xlabel 'Clock (hr)'; set ylabel 'Mb'; unset key; set title 'Sender frame size';  stats '$t0c' using (\$2/1e6) nooutput; p '-' u ((\$1/1e6)/3.6e3):(\$2/1e6) w l" &
 echo -n "Sender frame size bits: "
 cut -f2 -d' ' $t0c|get_moments
-#t0d=$(mktemp); echo "t0d $t0d"
-#grep -i "Estimated frame rate" $t|cut -f1,7 -d' ' > $t0d
-#cat $t0d|gnuplot -p -e "set xlabel 'Clock (hr)'; set ylabel 'Hz'; unset key; set title 'Sender Estimated frame rate'; p '-' u ((\$1/1e6)/3.6e3):2 w l" &
-#echo -n "Sender Estimated frame rate Hz: "
-#cut -f2 -d' ' $t0d|get_moments
-#t0e=$(mktemp); echo "t0e $t0e"
-#grep -i "Estimated bit rate" $t|grep MHz|cut -f1,7 -d' ' > $t0e
-#cat $t0e|gnuplot -p -e "set xlabel 'Clock (hr)'; set ylabel 'Mbps'; unset key; set title 'Sender Estimated bit rate'; p '-' u ((\$1/1e6)/3.6e3):2 w l" &
-#echo -n "Sender Estimated bit rate MHz: "
-#cut -f2 -d' ' $t0e|get_moments
 echo
 t0=$(mktemp); echo "t0 $t0"
-for i in {7003..7001}; do
+for i in $(seq "$base_port" "$hi_port"); do
     grep "cpu_sim ${i}" $t > $t0
-    echo "cpu_sim ${i}:"
+    echo "Component $(echo "$i - $base_port + 1" | bc):"
     echo -n "frame size bits: "; grep -i "Frame size" $t0|grep -v "simulate_stream"|grep -v Sending|cut -f 9 -d' '|get_moments
     t1=$(mktemp); echo "t1 $t1"; grep "Measured frame" $t0|grep -v "simulate_stream"|cut -f 1,8 -d' ' > $t1
-    gnuplot -p -e "set xlabel 'Clock (hr)'; set ylabel 'Hz'; unset key; set title '$i Measured frame rate';  stats '$t1' using 2 nooutput; p '-' u ((\$1/1e6)/3.6e3):2 w l" < $t1  &
+    gnuplot -p -e "set xlabel 'Clock (hr)'; set ylabel 'Hz'; unset key; set title 'Component $(echo "$i - $base_port + 1" | bc) Measured frame rate';  stats '$t1' using 2 nooutput; p '-' u ((\$1/1e6)/3.6e3):2 w l" < $t1  &
     echo -n "Measured frame rate Hz: "; cut -f 2 -d ' ' $t1 | get_moments 
     t2=$(mktemp); echo "t2 $t2"; grep "Measured bit" $t0|grep -v "simulate_stream"|cut -f 1,8 -d' '  > $t2
-    gnuplot -p -e "set xlabel 'Clock (hr)'; set ylabel 'Mbps'; unset key; set title '$i Measured bit rate';  stats '$t2' using 2 nooutput; p '-' u ((\$1/1e6)/3.6e3):2 w l"  < $t2 &
+    gnuplot -p -e "set xlabel 'Clock (hr)'; set ylabel 'Mbps'; unset key; set title 'Component $(echo "$i - $base_port + 1" | bc) Measured bit rate';  stats '$t2' using 2 nooutput; p '-' u ((\$1/1e6)/3.6e3):2 w l"  < $t2 &
     echo -n "Measured bit rate MHz: "; cut -f 2 -d ' ' $t2 | get_moments
-    grep Compute $t0|cut -f1,9 -d' '| gnuplot -p -e "set xlabel 'Clock (hr)'; set ylabel 'msec'; unset key; set title '$i compute latency'; p '-' u ((\$1/1e6)/3.6e3):(\$2/1e3) w l"
-    grep Compute $t0|cut -f1,12 -d' '|gnuplot -p -e "set xlabel 'Clock (hr)'; set ylabel 'usec'; unset key; set title '$i network latency'; p '-' u ((\$1/1e6)/3.6e3):2 w l"
-    #grep tsn $t0|cut -f 9,12 -d' '|gnuplot -p -e "unset key; set title '$i network latency usec vs size bits'; p '-' u ((\$1/1e6)/3.6e3):2"
-    #tg=$(mktemp); grep tsn $t0|cut -f 9,12 -d' ' > $tg; gnuplot -p -e "unset key; f(x) = a*x + b; fit f(x) '$tg' using 1:2 via a,b; set title '$i network latency usec vs size bits';  stats '$tg' using 2 nooutput; p '$tg' u 1:2, f(x) lc 'red'"
-    #tg=$(mktemp); grep tsn $t0|cut -f 9,12 -d' ' > $tg; gnuplot -p -e "unset key; f(x) = a*x + b; fit f(x) '$tg' using 1:2 via a,b; set title '$i network latency usec vs size bits'; set yrange [0:*]; p '$tg' u 1:2, f(x) lc 'red'" #headroom test
+    grep Compute $t0|cut -f1,9 -d' '| gnuplot -p -e "set xlabel 'Clock (hr)'; set ylabel 'msec'; unset key; set title 'Component $(echo "$i - $base_port + 1" | bc) compute latency'; p '-' u ((\$1/1e6)/3.6e3):(\$2/1e3) w l"
+    grep Compute $t0|cut -f1,12 -d' '|gnuplot -p -e "set xlabel 'Clock (hr)'; set ylabel 'usec'; unset key; set title 'Component $(echo "$i - $base_port + 1" | bc) network latency'; p '-' u ((\$1/1e6)/3.6e3):2 w l"
+    #grep tsn $t0|cut -f 9,12 -d' '|gnuplot -p -e "unset key; set title 'Component $(echo "$i - $base_port + 1" | bc) network latency usec vs size bits'; p '-' u ((\$1/1e6)/3.6e3):2"
+    #tg=$(mktemp); grep tsn $t0|cut -f 9,12 -d' ' > $tg; gnuplot -p -e "unset key; f(x) = a*x + b; fit f(x) '$tg' using 1:2 via a,b; set title 'Component $(echo "$i - $base_port + 1" | bc) network latency usec vs size bits';  stats '$tg' using 2 nooutput; p '$tg' u 1:2, f(x) lc 'red'"
+    #tg=$(mktemp); grep tsn $t0|cut -f 9,12 -d' ' > $tg; gnuplot -p -e "unset key; f(x) = a*x + b; fit f(x) '$tg' using 1:2 via a,b; set title 'Component $(echo "$i - $base_port + 1" | bc) network latency usec vs size bits'; set yrange [0:*]; p '$tg' u 1:2, f(x) lc 'red'" #headroom test
   echo
 done
 
@@ -65,41 +61,38 @@ t3=$(mktemp); echo "t3 $t3";
 t4=$(mktemp); echo "t4 $t4"; 
 t5=$(mktemp); echo "t5 $t5"; 
 
-#grep "\[cpu_sim 7003\]:  Measured frame rate" $t|cut -f 1,12 -d' '>$t3; grep -i "Estimated frame rate" $t|cut -f 1,9 -d' '>$t4; paste $t3 $t4|tee $t5|gnuplot -p -e "set title 'sent/recd'; p '-' u 1:2 w l lc 'red', '$t5' u 3:4 w l lc 'green'" &
-
-for i in {7003..7001}; do grep $i $t|grep recd|tail -1; done; echo; grep sent $t|tail -1; echo -n "Attempting: "; grep Attempting $t|wc -l; echo -n "dropped: "; grep dropped $t|wc -l
+for i in {$hi_port..$base_port}; do grep $i $t|grep recd|tail -1; done; echo; grep sent $t|tail -1; echo -n "Attempting: "; grep Attempting $t|wc -l; echo -n "dropped: "; grep dropped $t|wc -l
 
 t6=$(mktemp); echo "t6 $t6";
 t7=$(mktemp); echo "t7 $t7";
 grep "\[simulate_stream:] Sending frame" $t > $t6
 grep "\[cpu_sim 700[123]\]:  frame size" $t >> $t6
 grep done $t >> $t6
-#grep "Sending frame" $t >> $t6
 grep dropped $t >> $t6
 sort -k 1 -n $t6 > $t7
 
 t8=$(mktemp); echo "t8 $t8";
 grep "simulate_stream:] Sending frame" $t7 > $t8
-grep "cpu_sim 7003" $t7|grep done >> $t8
+grep "cpu_sim $hi_port" $t7|grep done >> $t8
 t9=$(mktemp); echo "t9 $t9";
 sort -k 1 -n $t8 > $t9
 
 t10=$(mktemp); echo "t10 $t10";
-grep "cpu_sim 7003]:  Sending" $t7 > $t10
+grep "cpu_sim $hi_port]:  Sending" $t7 > $t10
 grep "cpu_sim 7002" $t7|grep done >> $t10
 t11=$(mktemp); echo "t11 $t11";
 sort -k 1 -n $t10 > $t11
 
 t12=$(mktemp); echo "t12 $t12";
 grep "cpu_sim 7002]:  Sending" $t7 > $t12
-grep "cpu_sim 7001" $t7|grep done >> $t12
+grep "cpu_sim $base_port" $t7|grep done >> $t12
 t13=$(mktemp); echo "t13 $t13";
 sort -k 1 -n $t12 > $t13
 
-for i in {7003..7001}; do
+for i in $(seq "$base_port" "$hi_port"); do
     echo -n "Missed frame ratio for $i: "
     grep ratio $t|grep "cpu_sim $i"|cut -f 8 -d' '|tail -1
-    grep ratio $t|grep "cpu_sim $i"|cut -f 1,8 -d' '|gnuplot -p -e "set title 'Missed Frames $i'; unset key; set xlabel 'Clock (hr)'; set ylabel 'Fraction'; p '-' u ((\$1/1e6)/3.6e3):2 w l"
+    grep ratio $t|grep "cpu_sim $i"|cut -f 1,8 -d' '|gnuplot -p -e "set title 'Missed Frames Component $(echo "$i - $base_port + 1" | bc)'; unset key; set xlabel 'Clock (hr)'; set ylabel 'Fraction'; p '-' u ((\$1/1e6)/3.6e3):2 w l"
 done
 
 echo "t $t"
@@ -118,11 +111,6 @@ echo "t11 $t11"
 echo "t12 $t12"
 echo "t13 $t13"
 
-#search for missed frames
-tf=$(mktemp); echo -n "tf = "; echo $tf; grep simulate_stream $t|grep -Ev "^\[simulate_stream" |grep -v simulate_sender-zmq > $tf
-grep 700 $t|grep -Ev "^\[cpu_sim"|grep -v launcher_py_cpu_sim|grep -v simulate_stream >> $tf
-tf1=$(mktemp); echo -n "tf1 = "; echo $tf1; sort -n -k1 $tf|grep -v Estimated|grep -v Measured|grep -v recd|grep -v Memory > $tf1
-# then compare 7003 to 7002, etc.
 
 set -m
 
