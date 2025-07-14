5d9472687d346c3d6c51b88ce3f5590833ce02de
tsaie79 <tsaie79@gmail.com>
2025-07-14 13:31:44 +0000

    Update test.sh to switch from multi-GPU proxy to multi-mixed workflow
    - Changed the configuration file from `gpu_config.yml` to `rtdp/cylc/multi_mixed/example_config.yml`.
    - Updated the output workflow name from `gpu_workflow` to `mixed_workflow` to reflect the new workflow type.

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 -m rtdp.cli.rtdpcli generate --config gpu_config.yml --output gpu_workflow --workflow-type multi_gpu_proxy
-python3 -m rtdp.cli.rtdpcli run --workflow gpu_workflow
+python3 -m rtdp.cli.rtdpcli generate --config rtdp/cylc/multi_mixed/example_config.yml --output mixed_workflow --workflow-type multi_mixed
+python3 -m rtdp.cli.rtdpcli run --workflow mixed_workflow
